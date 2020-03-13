d20ca2a76c1e946e6cc82c89a69f0e58508f3a38
Nguyen Tran Cong <Catbui2805@gmail.com>
2020-03-14 00:00:20 +0700

    Init project

---
+++ ChangeAppIcon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChangeAppIcon
+//
+//  Created by Nguyen Tran Cong on 3/13/20.
+//  Copyright © 2020 Nguyen Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
